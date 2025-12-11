a4c2ccd836cccb0d7fb91c3011d65b547fb9de99
Prabash <mvsprabash@gmail.com>
2025-12-11 13:02:27 +0530

    Updated: Neovim Colorscheme config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,18 @@ return {
   -- install gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte | frappe | macchiato | mocha
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end
+  },
+
   -- set it as default
   {
     "LazyVim/LazyVim",
